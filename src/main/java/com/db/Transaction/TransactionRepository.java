54170faf573dccc77f56b88b95bf1a71c5e97298
karanpurswani <karanpurswani0723@gmail.com>
2019-10-09 19:16:37 +0530

    first

---
+++ src/main/java/com/db/Transaction/TransactionRepository.java
@@ -0,0 +1,16 @@
+package com.db.Transaction;
+
+import com.db.user.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends MongoRepository<Transaction, String> {
+
+    @Query("{ 'accountNumber' : ?0 }")
+    List<Transaction> findByAccountNumber(String accountNumber);
+
+}
---
+++ src/main/java/com/db/user/UserController.java
@@ -0,0 +1,44 @@
+package com.db.user;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "api/mongo/user")
+public class UserController {
+
+    @Autowired
+    UserServiceImpl userService;
+
+    private static long accountNumber=1000;
+
+    @RequestMapping(value = "/createUser",method = RequestMethod.POST)
+    public String create(@RequestBody User user) {
+
+        user.setAccountNumber(String.valueOf(accountNumber));
+        accountNumber++;
+        userService.create(user);
+        return "user is created.";
+    }
+
+
+
+    @GetMapping(value= "/getallUsers")
+    public Collection<User> getAll() {
+        return userService.findAll();
+    }
+
+
+    @GetMapping(value= "/getUserByAccountNumber/{account-number}")
+    public Optional<User> getByAccountNumber(@PathVariable(value= "account-number") String accountNumber) {
+        return userService.findByAccountNumber(accountNumber).stream().findFirst();
+    }
+
+
+}
